3b9d42e0737954a0b063a86c68681888723ba2e9
Anton Keks <anton@azib.net>
2016-12-27 21:42:37 +0200

    use same indentation as for regular properties

--- src/Track.kt
+++ src/Track.kt
@@ -3,16 +3,15 @@ import java.time.Instant
 import java.util.*
 
 data class Track(
-    val name: String,
-    val startAt: Instant,
-    val endAt: Instant,
-    val points: MutableList<TrackPoint> = ArrayList()
-) {
+  val name: String,
+  val startAt: Instant,
+  val endAt: Instant,
+  val points: MutableList<TrackPoint> = ArrayList()) {
   val duration = Duration.between(startAt, endAt)
 }
 
 data class TrackPoint(
-    val time: Instant,
-    val lat: Float,
-    val lon: Float
+  val time: Instant,
+  val lat: Float,
+  val lon: Float
 )
