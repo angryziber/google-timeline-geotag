8920ed5fdf06c84843e06637483dc824dc8722c1
Anton Keks <anton@azib.net>
2017-01-03 22:42:36 +0200

    have a nice toString()

--- src/geotag/timeline/TimeSpan.kt
+++ src/geotag/timeline/TimeSpan.kt
@@ -4,7 +4,7 @@ import java.time.Duration
 import java.time.Duration.between
 import java.time.Instant
 
-class TimeSpan(val begin: Instant, val end: Instant) {
+data class TimeSpan(val begin: Instant, val end: Instant) {
   val duration: Duration get() = between(begin, end)
   fun matches(dateTime: Instant) = begin <= dateTime && end >= dateTime
 }
