54c794cdba53ad9ac3da2a4f5f6b83f0bccb4792
Anton Keks <anton@azib.net>
2017-01-03 22:21:35 +0200

    output everything as a single track

--- src/geotag/out/GPX.kt
+++ src/geotag/out/GPX.kt
@@ -5,12 +5,14 @@ import java.io.File
 
 object GPX : Output {
   override fun start() = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<gpx xmlns="http://www.topografix.com/GPX/1/1">"""
+<gpx xmlns="http://www.topografix.com/GPX/1/1">
+<trk>
+<trkseg>"""
 
-  override fun write(file: File, point: TrackPoint) = """<trk>
-  <name>${file}</name>
-  <trkseg><trkpt lat="${point.lat.value}" lon="${point.lon.value}"><time>${point.time}</time></trkpt></trkseg>
-</trk>"""
+  override fun write(file: File, point: TrackPoint) =
+      """<trkpt lat="${point.lat.value}" lon="${point.lon.value}"><time>${point.time}</time><name>${file.name}</name><url>file://${file}</url></trkpt>"""
 
-  override fun end() = "</gpx>"
+  override fun end() = """</trkseg>
+</trk>
+</gpx>"""
 }
