ae256a0a549486acb9c9f89be122d5d5798228eb
Anton Keks <anton@azib.net>
2017-01-06 00:21:03 +0200

    trying to parse huge timeline json

--- src/geotag/timeline/JsonTimelineParser.kt
+++ src/geotag/timeline/JsonTimelineParser.kt
@@ -1,11 +1,46 @@
 package geotag.timeline
 
-import me.doubledutch.lazyjson.LazyObject
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.stream.JsonReader
 import java.io.File
+import java.io.IOException
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
 
 class JsonTimelineParser : TimelineParser {
+  val gson = Gson()
+
   override fun parse(path: File): List<Track> {
-    val obj = LazyObject(path.readText())
-    return emptyList()
+    val from = LocalDate.parse("2016-10-19").atStartOfDay(ZoneId.systemDefault()).toInstant()
+    val until = LocalDateTime.parse("2016-10-27T12:47:00").atZone(ZoneId.systemDefault()).toInstant()
+
+    val result = ArrayList<Track>(1000)
+
+    gson.newJsonReader(path.reader()).use { reader ->
+      reader.beginObject()
+      if (reader.nextName() != "locations") throw IOException("Json file must contain 'locations' array")
+      reader.beginArray()
+      while (reader.hasNext()) {
+        val o = reader.nextObject()
+        val time = o["timestampMs"].instant
+        if (time.isAfter(until)) continue
+        if (time.isBefore(from)) break
+        result += Track("", TimeSpan(time, time), listOf(
+            TrackPoint(o["latitudeE7"].e7, o["longitudeE7"].e7, time)
+        ))
+      }
+    }
+
+    return result
   }
+
+  fun JsonReader.nextObject(): JsonObject = gson.fromJson(this, JsonObject::class.java)
+
+  val JsonElement.instant: Instant get() = Instant.ofEpochMilli(asLong)
+  val JsonElement.e7: Float get() = asFloat / 10000000
 }
