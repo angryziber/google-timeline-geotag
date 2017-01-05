0f895813726c7e43709f91a36f1da9f0f9bfbb71
Anton Keks <anton@azib.net>
2017-01-05 20:42:34 +0200

    test for interpolation

--- test/geotag/timeline/TrackTest.kt
+++ test/geotag/timeline/TrackTest.kt
@@ -22,4 +22,16 @@ class TrackTest : Spek({
     val track = Track("x", TimeSpan(now(), now()), points)
     assertThat(track.pointAt(points[1].time)).isEqualTo(points[1])
   }
+
+  it("finds from a range of points") {
+    val points = listOf(
+        TrackPoint(0f, 0f, Instant.ofEpochSecond(0)),
+        TrackPoint(10f, -10f, Instant.ofEpochSecond(10)),
+        TrackPoint(20f, -20f, Instant.ofEpochSecond(20)),
+        TrackPoint(0f, 0f, Instant.ofEpochSecond(30))
+    )
+    val track = Track("x", TimeSpan(now(), now()), points)
+    assertThat(track.pointAt(Instant.ofEpochSecond(12)))
+        .isEqualTo(TrackPoint(12f, -12f, Instant.ofEpochSecond(12)))
+  }
 })
