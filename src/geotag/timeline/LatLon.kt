ae615aceaa95e7b2bbd5c3a3d4c6cdc0c1b2d284
Anton Keks <anton@azib.net>
2017-01-05 20:18:44 +0200

    use one-liner getters

--- src/geotag/timeline/LatLon.kt
+++ src/geotag/timeline/LatLon.kt
@@ -6,15 +6,10 @@ abstract class LatLon(val value: Float) {
   val abs = abs(value)
 
   abstract val ref: Char
-
-  val d: Int
-    get() = abs.toInt()
-
-  val m: Int
-    get() = ((abs - d) * 60f).toInt()
-
-  val s: Float
-    get() = (abs - d - m/60f) * 3600f
+  
+  val d: Int get() = abs.toInt()
+  val m: Int get() = ((abs - d) * 60f).toInt()
+  val s: Float get() = (abs - d - m/60f) * 3600f
 
   override fun equals(other: Any?) = other is LatLon && value == other.value
   override fun hashCode() = value.hashCode()
